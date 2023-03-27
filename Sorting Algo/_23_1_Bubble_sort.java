d8c10b22e2944f5e170f29d085a6f8fd86e9b76b
Amit1198911 <97092851+Amit1198911@users.noreply.github.com>
2023-03-27 22:13:54 +0530

    Add files via upload

---
+++ Sorting Algo/_23_1_Bubble_sort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class _23_1_Bubble_sort {
+    public static void bubble_Sort(int arr[]){
+        int n = arr.length;
+        for(int turn=0;turn<n-1;turn++){
+            for(int j=0;j<n-1-turn;j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]=temp;
+
+                }
+
+            }
+        }
+
+    }
+
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+
+    }
+    public static void main(String args[]){
+        int arr[] = {5, 4, 1, 3, 2};
+        bubble_Sort(arr);
+        printArr(arr);  
+
+    }
+}
